1495cef5109733215eff902ee80ee2c0146cc023
Granit Zymeri <gzymeri55@gmail.com>
2022-05-19 17:22:18 -0400

    done

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,9 @@
 function findShortestString(arr) {
   // type your code here
+  let shortest_string =   arr.reduce(function(a, b) {
+      return a.length <= b.length ? a : b;
+  })
+  return shortest_string
 }
 
 if (require.main === module) {
